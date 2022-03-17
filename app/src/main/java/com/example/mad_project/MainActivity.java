80536883ae1682d7890d0566bb26e0471a1a22ca
PoornaSankalana <sanka2016pps@gmail.com>
2022-03-17 11:48:10 +0530

    Implementing Signin UI

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.mad_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
-public class MainActivity extends AppCompatActivity {
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity{
+
+    ImageView googleLogin;
+    Button signinBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        googleLogin = findViewById(R.id.google_login);
+        signinBtn = findViewById(R.id.signinButton);
+
+        googleLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Sign in Successfull", Toast.LENGTH_SHORT). show();
+            }
+        });
     }
 }
