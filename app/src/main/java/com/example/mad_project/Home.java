b1a6c3dd0554244f556b31a672b784b96ffa5959
Poorna Sankalana <sanka2016pps@gmail.com>
2022-04-23 16:23:44 +0530

    implementing google login and sign out functions

--- app/src/main/java/com/example/mad_project/Home.java
+++ app/src/main/java/com/example/mad_project/Home.java
@@ -1,11 +1,21 @@
 package com.example.mad_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,7 +23,13 @@ public class Home<placeholder> extends AppCompatActivity {
 
     BottomNavigationItemView placeHolder;
     BottomNavigationView bottomNavView;
+    TextView username;
+    ImageView logout;
 
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +40,37 @@ public class Home<placeholder> extends AppCompatActivity {
 
         bottomNavView.setBackground(null);
         placeHolder.setClickable(false);
+
+        username = findViewById(R.id.name);
+        logout = findViewById(R.id.logout);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account != null){
+            String Name = account.getDisplayName();
+
+            username.setText("Hello " + Name);
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignOut();
+            }
+        });
+    }
+
+    private void SignOut() {
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
     }
 }
