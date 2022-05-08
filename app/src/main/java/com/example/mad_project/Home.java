29cafa148923c449564d384649a5504b1498b3de
Poorna Sankalana <sanka2016pps@gmail.com>
2022-05-08 13:14:39 +0530

    Home Java class

--- app/src/main/java/com/example/mad_project/Home.java
+++ app/src/main/java/com/example/mad_project/Home.java
@@ -2,14 +2,17 @@ package com.example.mad_project;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -22,12 +25,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class Home<placeholder> extends AppCompatActivity {
 
     BottomNavigationItemView placeHolder;
-    BottomNavigationView bottomNavView;
-    TextView username;
-    ImageView logout;
+    BottomNavigationView bottomNavViewer;
+    TextView username, header;
 
+    ImageView logout;
+    String UserName;
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
+    private long backTime;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -35,14 +40,17 @@ public class Home<placeholder> extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new HomeFragment(), "HomeFragment").commit();
+
+        header = findViewById(R.id.GreetingText);
         placeHolder = findViewById(R.id.placeholder);
-        bottomNavView = findViewById(R.id.bottomNavView);
-
-        bottomNavView.setBackground(null);
-        placeHolder.setClickable(false);
-
+        bottomNavViewer = findViewById(R.id.bottomNavView);
         username = findViewById(R.id.name);
         logout = findViewById(R.id.logout);
+
+        bottomNavViewer.setBackground(null);
+        placeHolder.setClickable(false);
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(this, gso);
@@ -53,6 +61,12 @@ public class Home<placeholder> extends AppCompatActivity {
             String Name = account.getDisplayName();
 
             username.setText("Hello " + Name);
+            UserName = Name;
+        } else {
+            Bundle extras = getIntent().getExtras();
+            String user_name = extras.getString("Username");
+            username.setText("Hello " + user_name);
+            UserName = user_name;
         }
 
         logout.setOnClickListener(new View.OnClickListener() {
@@ -61,6 +75,18 @@ public class Home<placeholder> extends AppCompatActivity {
                 SignOut();
             }
         });
+
+        bottomNavViewer.setOnNavigationItemSelectedListener(Lister);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(backTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+        } else {
+            Toast.makeText(this, "Press Again to Exit", Toast.LENGTH_SHORT).show();
+        }
+        backTime = System.currentTimeMillis();
     }
 
     private void SignOut() {
@@ -73,4 +99,47 @@ public class Home<placeholder> extends AppCompatActivity {
         });
 
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener Lister = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        String Fragment;
+        @SuppressLint("SetTextI18n")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.home:
+                    header.setText("Welcome...");
+                    username.setText("Hello " + UserName);
+                    selectedFragment = new HomeFragment();
+                    Fragment = "HomeFragment";
+                    break;
+
+                case R.id.offer:
+                    header.setText("Offers");
+                    username.setText("Here is our offers");
+                    selectedFragment = new OffersFragment();
+                    Fragment = "offerFragment";
+                    break;
+
+                case R.id.menu:
+                    header.setText("Menu");
+                    username.setText("Select Your Category Here");
+                    selectedFragment = new MenuFragment();
+                    Fragment = "MenuFragment";
+                    break;
+
+                case R.id.more:
+                    header.setText("More");
+                    username.setText("More Options for User");
+                    selectedFragment = new MoreFragment();
+                    Fragment = "MoreFragment";
+                    break;
+            }
+            
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, selectedFragment, Fragment).commit();
+
+            return true;
+        }
+    };
 }
